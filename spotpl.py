03b453825f4cf7d0d2a1e4f7ddf06b4ba353843a
Robert J <chew-z@users.noreply.github.com>
2017-06-04 21:57:22 +0700

    Spotify Token

--- spotpl.py
+++ spotpl.py
@@ -468,8 +468,8 @@ if __name__ == '__main__':
     spotify_uri = args.playlist
 
     oa2 = oauth2.SpotifyClientCredentials(
-        client_id='768998a7e3444c6a82f0c11ddd59c946',
-        client_secret='4b1e7672e14a42269ee4a22e11214fdf')
+        client_id='4fe3fecfe5334023a1472516cc99d805',
+        client_secret='0f02b7c483c04257984695007a4a8d5c')
     token = oa2.get_access_token()
     sp = spotipy.Spotify(auth=token)
 
