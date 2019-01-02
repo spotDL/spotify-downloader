b58c4775f2050bf48566fa43248ef983d741d0ff
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2019-01-02 19:22:50 +0000

    Run black formatter on the whole codebase (#460)

--- test/test_download_with_metadata.py
+++ test/test_download_with_metadata.py
@@ -178,18 +178,21 @@ class TestFFmpeg:
         assert " ".join(command) == expect_command
 
     def test_correct_container_for_m4a(self, filename_fixture, monkeypatch):
-        expect_command = "ffmpeg -y -hide_banner -nostats -v panic -i {0}.m4a.temp -acodec copy -b:a 192k -vn {0}.m4a".format(os.path.join(const.args.folder, filename_fixture))
+        expect_command = "ffmpeg -y -hide_banner -nostats -v panic -i {0}.m4a.temp -acodec copy -b:a 192k -vn {0}.m4a".format(
+            os.path.join(const.args.folder, filename_fixture)
+        )
         _, command = convert.song(
             filename_fixture + ".m4a", filename_fixture + ".m4a", const.args.folder
         )
-        assert ' '.join(command) == expect_command
+        assert " ".join(command) == expect_command
 
 
 class TestAvconv:
     def test_convert_from_m4a_to_mp3(self, filename_fixture, monkeypatch):
         monkeypatch.setattr("os.remove", lambda x: None)
         expect_command = "avconv -loglevel 0 -i {0}.m4a -ab 192k {0}.mp3 -y".format(
-            os.path.join(const.args.folder, filename_fixture))
+            os.path.join(const.args.folder, filename_fixture)
+        )
         _, command = convert.song(
             filename_fixture + ".m4a",
             filename_fixture + ".mp3",
