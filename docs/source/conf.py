aebc11ed17015a05700fdeb72ee74f70f9756568
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2020-06-13 19:04:57 +0530

    Explicitly state master file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ release = __version__
 
 # -- General configuration ---------------------------------------------------
 
+# Entry file
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
