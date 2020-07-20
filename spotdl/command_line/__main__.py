c4612bd9c42aa4579cdf5314bbb8069c21e9d178
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2020-07-20 23:18:20 +0530

    Show current version in debug log

--- spotdl/command_line/__main__.py
+++ spotdl/command_line/__main__.py
@@ -6,6 +6,7 @@ from spotdl.command_line import arguments
 from spotdl.command_line.exceptions import ArgumentError
 
 import spotdl.command_line.exitcodes
+import spotdl.version
 
 # hardcode loglevel for dependencies so that they do not spew generic
 # log messages along with spotdl.
@@ -20,6 +21,7 @@ def main():
         sys.exit(spotdl.command_line.exitcodes.ARGUMENT_ERROR)
     else:
         args = parser.parse_args().__dict__
+        logger.debug("spotdl {}".format(spotdl.version.__version__))
     try:
         with Spotdl(args) as spotdl_handler:
             exitcode = spotdl_handler.match_arguments()
