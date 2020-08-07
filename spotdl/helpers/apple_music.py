33b754cc9685e5cff89e1124ea606750cce23ec4
Björn Plüster <bjoern.pl@outlook.de>
2020-08-07 14:16:00 +0200

    Changed log level and fixed dictionary keys to ensure compatibility with spotify playlist dictionary format.

--- spotdl/helpers/apple_music.py
+++ spotdl/helpers/apple_music.py
@@ -16,25 +16,33 @@ def fetch_playlist(playlist_uri):
     else:
         playlist_html = BeautifulSoup(content.read(), 'html.parser')
         playlist = {
-            'items': [],
+            'tracks': {
+                "items": [],
+                "total": 0,
+                "next": None
+            },
             'name': playlist_html.find("h1", {"class": "product-name"}).text.strip(),
             'creator': playlist_html.find("h2", {"class": "product-creator"}).text.strip()
         }
 
-        logger.debug(f'Fetching list: "{playlist["name"]} by {playlist["creator"]}"')
+        logger.info(f'Fetching list: "{playlist["name"]} by {playlist["creator"]}" from Apple Music')
 
         songs = playlist_html.findAll("div", {"class": "song"})
         if not songs:
             logger.error(f'Playlist {playlist_uri} is either empty or some other error occurred.')
             raise spotdl.helpers.exceptions.AppleMusicPlaylistNotFoundError
+        playlist['tracks']['total'] = len(songs)
         for song in songs:
             try:
                 song_dict = {
                     'name': song.find('div', attrs={'class': 'song-name'}).text.strip(),
-                    'artists': [song.find('a', attrs={'class': 'dt-link-to'}).text.strip()]
+                    'artists': [
+                        {'name': song.find('a', attrs={'class': 'dt-link-to'}).text.strip()}
+                    ]
                 }
             except AttributeError:
                 logger.warning('Song data not found in html. Skipping')
                 pass
             else:
-                playlist['items'].append(song_dict)
+                playlist['tracks']['items'].append(song_dict)
+    return playlist
