dcd430d6111d7c3a6e4dde1026bb3547c312c2e7
Gabriel Minossi <gabriel.minossi@gmail.com>
2020-08-10 19:02:08 -0300

    Patch pytube from KeyError adaptiveFormats

--- spotdl/patch/youtube.py
+++ spotdl/patch/youtube.py
@@ -35,6 +35,9 @@ import pytube
 from pytube import request
 from pytube.extract import get_ytplayer_config, apply_signature, js_url
 
+import logging
+logger = logging.getLogger(__name__)
+
 def apply_patches():
     """
     This methods applies all the needed patches on PyTube.
@@ -74,12 +77,14 @@ def apply_descrambler(stream_data, key):
     if key == "url_encoded_fmt_stream_map" and not stream_data.get(
         "url_encoded_fmt_stream_map"
     ):
-        formats = json.loads(stream_data["player_response"])["streamingData"]["formats"]
-        formats.extend(
-            json.loads(stream_data["player_response"])["streamingData"][
-                "adaptiveFormats"
-            ]
-        )
+        formats = []
+        streamingData = json.loads(stream_data["player_response"])["streamingData"]
+        if 'formats' in streamingData.keys():
+            logger.debug("Stream data has formats")
+            formats.extend(json.loads(stream_data["player_response"])["streamingData"]["formats"])
+        if 'adaptiveFormats' in streamingData.keys():
+            logger.debug("Stream data has adaptiveFormats")
+            formats.extend(json.loads(stream_data["player_response"])["streamingData"]["adaptiveFormats"])
         try:
             stream_data[key] = [
                 {
@@ -110,6 +115,7 @@ def apply_descrambler(stream_data, key):
                 for i, format_item in enumerate(formats)
             ]
     else:
+        logger.debug("else")
         stream_data[key] = [
             {k: unquote(v) for k, v in parse_qsl(i)}
             for i in stream_data[key].split(",")
