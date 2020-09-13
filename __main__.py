f0ba2a9d56229799b6ad009aa8a4b2fe790f057f
Mikhail Zex <52100648+Mikhail-Zex@users.noreply.github.com>
2020-09-14 00:47:05 +0530

    ad-hock CLI usage comments?

--- __main__.py
+++ __main__.py
@@ -9,7 +9,39 @@ from spotdl.search.songObj import SongObj
 #! The actual download stuff
 from spotdl.download.downloader import DownloadManager
 
+#! to avoid packaging errors
+from multiprocessing import freeze_support
+
+
+#! Usage is simple - call 'python __main__.py <links, search terms, tracking files seperated by spaces>
+#! Eg.
+#!      python __main__.py https://open.spotify.com/playlist/37i9dQZF1DWXhcuQw7KIeM?si=xubKHEBESM27RqGkqoXzgQ 'old gods of asgard Control' https://open.spotify.com/album/2YMWspDGtbDgYULXvVQFM6?si=gF5dOQm8QUSo-NdZVsFjAQ https://open.spotify.com/track/08mG3Y1vljYA6bvDt4Wqkj?si=SxezdxmlTx-CaVoucHmrUA
+#!
+#! Well, yeah its a pretty long example but, in theory, it should work like a charm. 
+#!
+#! A '.spotdlTrackingFile' is automatically  created with the name of the first song in the playlist/album or
+#! the name of the song supplied. We don't really re re re-query YTM and SPotify as all relevant details are
+#! stored to disk.
+#!
+#! Files are cleaned up on download failure.
+#!
+#! All songs are normalized to standard base volume. the soft ones are made louder, the loud ones, softer.
+#!
+#! The progress bar is synched across multiple-processes (4 processes as of now), getting the progress bar to
+#! synch was an absolute pain, each process knows how much 'it' progressed, but the display has to be for the
+#! overall progress so, yeah... that took time.
+#!
+#! spotdl will show you its true speed on longer download's - so make sure you try downloading a playlist.
+#!
+#! still yet to try and package this but, in theory, there should be no errors.
+#!
+#!                                                          - cheerio! (Michael)
+#!
+#! P.S. Tell me what you think. Up to your expectations?
+
 if __name__ == '__main__':
+    freeze_support()
+
     initialize(
         clientId='4fe3fecfe5334023a1472516cc99d805',
         clientSecret='0f02b7c483c04257984695007a4a8d5c'
