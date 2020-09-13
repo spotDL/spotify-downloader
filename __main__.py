c157e96bd3f6a119821e74d5e19c5a36dc3f48a2
Mikhail Zex <52100648+Mikhail-Zex@users.noreply.github.com>
2020-09-14 01:25:49 +0530

    CLI error fix

--- __main__.py
+++ __main__.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
             print('Fetching Playlist...')
             songObjList = get_playlist_tracks(request)
 
-            downloader.download_multiple_songs(request)
+            downloader.download_multiple_songs(songObjList)
         
         elif request.endswith('.spotdlTrackingFile'):
             print('Preparing to resume download...')
