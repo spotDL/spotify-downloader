a4894722a400f63cfe770108edfbe9e4764c941e
phcreery <phcreery@gmail.com>
2020-10-20 12:20:25 -0500

    more theme options

--- spotdl/cli/displayManager.py
+++ spotdl/cli/displayManager.py
@@ -43,15 +43,19 @@ from rich.text import Text
 #==============
 
 custom_theme = Theme({
-    "general" : "green",
-    "nonimportant" : "rgb(40,100,40)",
-    "progress.data.speed" : "red",
-    "progress.description" : "none",
-    "progress.download" : "green",
-    "progress.filesize" : "green",
-    "progress.filesize.total" : "green",
-    "progress.percentage" : "green",
-    "progress.remaining" : "rgb(40,100,40)"
+    "bar.back"                  : "grey23",
+    "bar.complete"              : "rgb(165,66,129)",
+    "bar.finished"              : "rgb(114,156,31)",
+    "bar.pulse"                 : "rgb(165,66,129)",
+    "general"                   : "green",
+    "nonimportant"              : "rgb(40,100,40)",
+    "progress.data.speed"       : "red",
+    "progress.description"      : "none",
+    "progress.download"         : "green",
+    "progress.filesize"         : "green",
+    "progress.filesize.total"   : "green",
+    "progress.percentage"       : "green",
+    "progress.remaining"        : "rgb(40,100,40)"
 })
 
 
