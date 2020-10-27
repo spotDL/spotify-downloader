dc8ea44a6d3afc73dcc3614ec24610b60377368f
Alp Sayin <alpsayin@gmail.com>
2020-10-28 00:25:32 +0300

    reverting walrused statement to 2 lines for backwards python3 compat

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -80,8 +80,8 @@ def download_song(songObj: SongObj, displayManager: DisplayManager = None,
     #! like to retain their equivalents, so they aren't removed in the prior loop
     convertedFileName = convertedFileName.replace('"', "'").replace(': ', ' - ')
     #! if a songObj's playlistIndex is not None then we prepend it to keep the playlist/album order
-    if (playlistIndex := songObj.get_playlist_index()) is not None:
-        convertedFileName = f'{playlistIndex:04d}' + ' - ' + convertedFileName
+    playlistIndex = songObj.get_playlist_index()
+    if playlistIndex is not None:
 
     convertedFilePath = join('.', convertedFileName) + '.mp3'
 
