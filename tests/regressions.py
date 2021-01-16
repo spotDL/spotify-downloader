93dcd5b729b5c2ad788113f14c3ff53ebc2d44ea
Andrzej Klajnert <github@aklajnert.pl>
2021-01-16 23:10:06 +0100

    Make `__parse_duration()` function less sensible to errors (#1126)
    Author - @aklajnert 
    
    * Make `__parse_duration()` function less sensible to errors.
    
    * Add regression tests
    
    * Install `ffmpeg` on actions
    
    * Add missing `sudo`
    
    * Run `ffmpeg --help` to see if it's available
    
    * Remove regressions from CI for now, as they don't work

---
+++ tests/regressions.py
@@ -0,0 +1,22 @@
+import sys
+
+from spotdl import console_entry_point
+
+SONGS = {
+    "https://open.spotify.com/track/6CN3e26iQSj1N5lomh0mfO": "Eminem - Like Toy Soldiers.mp3",
+    "https://open.spotify.com/track/3bNv3VuUOKgrf5hu3YcuRo": "Adele - Someone Like You.mp3",
+}
+
+
+def test_regressions(monkeypatch, tmpdir):
+    """
+    Download songs that caused problems in the past, to make sure they won't happen again.
+    """
+    monkeypatch.chdir(tmpdir)
+    monkeypatch.setattr(sys, "argv", ["dummy", *SONGS.keys()])
+
+    console_entry_point()
+
+    assert sorted(
+        [file.basename for file in tmpdir.listdir() if file.isfile()]
+    ) == sorted([*SONGS.values()])
