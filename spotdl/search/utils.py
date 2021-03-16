38d6f7dd260cee6fc24315dfa3ee226c8da507f6
Jakub <42355410+xnetcat@users.noreply.github.com>
2021-03-16 07:27:34 +0100

    get all artist tracks (#1208)
    Authored by @xnetcat

--- spotdl/search/utils.py
+++ spotdl/search/utils.py
@@ -65,6 +65,71 @@ def get_album_tracks(albumUrl: str) -> List[SongObj]:
     return albumTracks
 
 
+def get_artist_tracks(artistUrl: str) -> List[SongObj]:
+    '''
+    `str` `albumUrl` : Spotify Url of the artist whose tracks are to be
+    retrieved
+
+    returns a `list<songObj>` containing Url's of each track in the artist profile
+    '''
+
+    spotifyClient = get_spotify_client()
+    artistTracks = []
+    offset = 0
+
+    artistResponse = spotifyClient.artist_albums(artistUrl)
+
+    # while loop acts like do-while
+    while True:
+        for album in artistResponse['items']:
+            # get albums and singles
+            if not (
+                album['album_group'] == 'appears_on' and album['album_type'] in [
+                    'album', 'compilation']
+            ):
+                artistTracks.extend(get_album_tracks(album['id']))
+            # get features from other artists albums
+            elif album['album_group'] == 'appears_on' and album['album_type'] == 'album':
+                trackResponse = spotifyClient.album_tracks(album['uri'])
+                albumTracks = []
+
+                # while loop acts like do-while
+                while True:
+                    for track in trackResponse['items']:
+                        for artist in track['artists']:
+                            if artist['id'] == artistResponse['href'].split('/')[-2]:
+                                song = SongObj.from_url(
+                                    'https://open.spotify.com/track/' + track['id']
+                                )
+
+                                if song.get_youtube_link() is not None:
+                                    albumTracks.append(song)
+
+                    # check if more tracks are to be passed
+                    if trackResponse['next']:
+                        trackResponse = spotifyClient.album_tracks(
+                            album['uri'],
+                            offset=len(albumTracks)
+                        )
+                    else:
+                        break
+
+                artistTracks.extend(albumTracks)
+
+        offset += len(artistResponse['items'])
+
+        # check if more albums are to be passed
+        if artistResponse['next']:
+            artistResponse = spotifyClient.artist_albums(
+                artistUrl,
+                offset=offset
+            )
+        else:
+            break
+
+    return artistTracks
+
+
 def get_playlist_tracks(playlistUrl: str) -> List[SongObj]:
     '''
     `str` `playlistUrl` : Spotify Url of the album whose tracks are to be
