663bfb00cfce2cdcfe6d1a58ec193d19d2846644
xnetcat <42355410+xnetcat@users.noreply.github.com>
2021-04-08 08:26:58 +0200

    save some memory in case the output will be big

--- spotdl/download/ffmpeg.py
+++ spotdl/download/ffmpeg.py
@@ -7,19 +7,21 @@ import re
 def has_correct_version(skip_version_check: bool = False) -> bool:
     process = subprocess.Popen(
         ['ffmpeg', '-version'],
-        shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8"
     )
 
-    proc_out, proc_err = process.communicate()
-
-    out = proc_out + proc_err
+    output = "".join(process.communicate())
 
     if process.returncode == 127:
         print("FFmpeg was not found, spotDL cannot continue.", file=sys.stderr)
         return False
 
     if skip_version_check is False:
-        result = re.search(r"ffmpeg version \w?(\d+\.)?(\d+)", out.decode("utf-8"))
+        result = re.search(r"ffmpeg version \w?(\d+\.)?(\d+)", output)
 
         if result is None:
             print("Your FFmpeg version couldn't be detected", file=sys.stderr)
