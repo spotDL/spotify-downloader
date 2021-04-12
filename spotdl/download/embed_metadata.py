94d5f02616efa258140dc20ceef628ea6b478f8a
xnetcat <42355410+xnetcat@users.noreply.github.com>
2021-04-12 15:24:03 +0200

    pep8: embed_metadata

--- spotdl/download/embed_metadata.py
+++ spotdl/download/embed_metadata.py
@@ -39,102 +39,102 @@ for key in M4A_TAG_PRESET.keys():
     TAG_PRESET[key] = key
 
 
-def _set_id3_mp3(convertedFilePath, songObj):
+def _set_id3_mp3(converted_file_path, song_obj):
     # embed song details
     # ! we save tags as both ID3 v2.3 and v2.4
     # ! The simple ID3 tags
-    audioFile = EasyID3(convertedFilePath)
+    audio_file = EasyID3(converted_file_path)
 
-    _embed_mp3_metadata(audioFile, songObj, convertedFilePath)
+    _embed_mp3_metadata(audio_file, song_obj, converted_file_path)
 
-    audioFile.save(v2_version=3)
+    audio_file.save(v2_version=3)
 
 
-def _set_id3_m4a(convertedFilePath, songObj):
+def _set_id3_m4a(converted_file_path, song_obj):
     # embed song details
     # ! we save tags as both ID3 v2.3 and v2.4
     # ! The simple ID3 tags
-    audioFile = MP4(convertedFilePath)
+    audio_file = MP4(converted_file_path)
 
-    _embed_basic_metadata(audioFile, songObj, "m4a", M4A_TAG_PRESET)
-    _embed_m4a_metadata(audioFile, songObj)
+    _embed_basic_metadata(audio_file, song_obj, "m4a", M4A_TAG_PRESET)
+    _embed_m4a_metadata(audio_file, song_obj)
 
-    audioFile.save()
-
-
-def _set_id3_flac(convertedFilePath, songObj):
-    audioFile = FLAC(convertedFilePath)
-
-    _embed_basic_metadata(audioFile, songObj, "flac")
-    _embed_ogg_metadata(audioFile, songObj)
-    _embed_cover(audioFile, songObj, "flac")
-
-    audioFile.save()
+    audio_file.save()
 
 
-def _set_id3_opus(convertedFilePath, songObj):
-    audioFile = OggOpus(convertedFilePath)
+def _set_id3_flac(converted_file_path, song_obj):
+    audio_file = FLAC(converted_file_path)
 
-    _embed_basic_metadata(audioFile, songObj, "opus")
-    _embed_ogg_metadata(audioFile, songObj)
-    _embed_cover(audioFile, songObj, "opus")
+    _embed_basic_metadata(audio_file, song_obj, "flac")
+    _embed_ogg_metadata(audio_file, song_obj)
+    _embed_cover(audio_file, song_obj, "flac")
 
-    audioFile.save()
-
-
-def _set_id3_ogg(convertedFilePath, songObj):
-    audioFile = OggVorbis(convertedFilePath)
-
-    _embed_basic_metadata(audioFile, songObj, "ogg")
-    _embed_ogg_metadata(audioFile, songObj)
-    _embed_cover(audioFile, songObj, "ogg")
-
-    audioFile.save()
+    audio_file.save()
 
 
-def _embed_mp3_metadata(audioFile, songObj, convertedFilePath):
+def _set_id3_opus(converted_file_path, song_obj):
+    audio_file = OggOpus(converted_file_path)
+
+    _embed_basic_metadata(audio_file, song_obj, "opus")
+    _embed_ogg_metadata(audio_file, song_obj)
+    _embed_cover(audio_file, song_obj, "opus")
+
+    audio_file.save()
+
+
+def _set_id3_ogg(converted_file_path, song_obj):
+    audio_file = OggVorbis(converted_file_path)
+
+    _embed_basic_metadata(audio_file, song_obj, "ogg")
+    _embed_ogg_metadata(audio_file, song_obj)
+    _embed_cover(audio_file, song_obj, "ogg")
+
+    audio_file.save()
+
+
+def _embed_mp3_metadata(audio_file, song_obj, converted_file_path):
 
     # ! Get rid of all existing ID3 tags (if any exist)
-    audioFile.delete()
+    audio_file.delete()
 
     # ! song name
-    audioFile['title'] = songObj.get_song_name()
-    audioFile['titlesort'] = songObj.get_song_name()
+    audio_file['title'] = song_obj.get_song_name()
+    audio_file['titlesort'] = song_obj.get_song_name()
 
     # ! track number
-    audioFile['tracknumber'] = str(songObj.get_track_number())
+    audio_file['tracknumber'] = str(song_obj.get_track_number())
 
     # ! disc number
-    audioFile['discnumber'] = str(songObj.get_disc_number())
+    audio_file['discnumber'] = str(song_obj.get_disc_number())
 
     # ! genres (pretty pointless if you ask me)
     # ! we only apply the first available genre as ID3 v2.3 doesn't support multiple
     # ! genres and ~80% of the world PC's run Windows - an OS with no ID3 v2.4 support
-    genres = songObj.get_genres()
+    genres = song_obj.get_genres()
     if len(genres) > 0:
-        audioFile['genre'] = genres[0]
+        audio_file['genre'] = genres[0]
 
     # ! all involved artists
-    audioFile['artist'] = songObj.get_contributing_artists()
+    audio_file['artist'] = song_obj.get_contributing_artists()
 
     # ! album name
-    audioFile['album'] = songObj.get_album_name()
+    audio_file['album'] = song_obj.get_album_name()
 
     # ! album artist (all of 'em)
-    audioFile['albumartist'] = songObj.get_album_artists()
+    audio_file['albumartist'] = song_obj.get_album_artists()
 
     # ! album release date (to what ever precision available)
-    audioFile['date'] = songObj.get_album_release()
-    audioFile['originaldate'] = songObj.get_album_release()
+    audio_file['date'] = song_obj.get_album_release()
+    audio_file['originaldate'] = song_obj.get_album_release()
 
     # ! save as both ID3 v2.3 & v2.4 as v2.3 isn't fully features and
     # ! windows doesn't support v2.4 until later versions of Win10
-    audioFile.save(v2_version=3)
+    audio_file.save(v2_version=3)
 
     # ! setting the album art
-    audioFile = ID3(convertedFilePath)
-    rawAlbumArt = urlopen(songObj.get_album_cover_url()).read()
-    audioFile['APIC'] = AlbumCover(
+    audio_file = ID3(converted_file_path)
+    rawAlbumArt = urlopen(song_obj.get_album_cover_url()).read()
+    audio_file['APIC'] = AlbumCover(
         encoding=3,
         mime='image/jpeg',
         type=3,
@@ -143,118 +143,118 @@ def _embed_mp3_metadata(audioFile, songObj, convertedFilePath):
     )
 
     # ! setting the lyrics
-    lyrics = songObj.get_lyrics()
+    lyrics = song_obj.get_lyrics()
     USLTOutput = USLT(encoding=3, lang=u'eng', desc=u'desc', text=lyrics)
-    audioFile["USLT::'eng'"] = USLTOutput
+    audio_file["USLT::'eng'"] = USLTOutput
 
 
-def _embed_m4a_metadata(audioFile, songObj):
+def _embed_m4a_metadata(audio_file, song_obj):
     # set year
-    year = songObj.get_album_release().split("-")[0]
+    year = song_obj.get_album_release().split("-")[0]
     if year:
-        audioFile[M4A_TAG_PRESET["year"]] = year
+        audio_file[M4A_TAG_PRESET["year"]] = year
 
     # set youtube link as comment
-    youtube_link = songObj.get_youtube_link()
+    youtube_link = song_obj.get_youtube_link()
     if youtube_link:
-        audioFile[M4A_TAG_PRESET["comment"]] = youtube_link
+        audio_file[M4A_TAG_PRESET["comment"]] = youtube_link
 
     # set lyrics
-    lyrics = songObj.get_lyrics()
+    lyrics = song_obj.get_lyrics()
     if lyrics:
-        audioFile[M4A_TAG_PRESET["lyrics"]] = lyrics
+        audio_file[M4A_TAG_PRESET["lyrics"]] = lyrics
 
     # Explicit values: Dirty: 4, Clean: 2, None: 0
-    audioFile[M4A_TAG_PRESET["explicit"]] = (0,)
+    audio_file[M4A_TAG_PRESET["explicit"]] = (0,)
     try:
-        audioFile[M4A_TAG_PRESET["albumart"]] = [
-            MP4Cover(urlopen(songObj.get_album_cover_url()).read(),
+        audio_file[M4A_TAG_PRESET["albumart"]] = [
+            MP4Cover(urlopen(song_obj.get_album_cover_url()).read(),
                      imageformat=MP4Cover.FORMAT_JPEG)
         ]
     except IndexError:
         pass
 
 
-def _embed_basic_metadata(audioFile, songObj, encoding, preset=TAG_PRESET):
+def _embed_basic_metadata(audio_file, song_obj, encoding, preset=TAG_PRESET):
 
     # set main artist
-    main_artist = songObj.get_contributing_artists()[0]
+    main_artist = song_obj.get_contributing_artists()[0]
     if main_artist:
-        audioFile[preset["artist"]] = main_artist
-        audioFile[preset["albumartist"]] = main_artist
+        audio_file[preset["artist"]] = main_artist
+        audio_file[preset["albumartist"]] = main_artist
 
     # set song title
-    song_title = songObj.get_song_name()
+    song_title = song_obj.get_song_name()
     if song_title:
-        audioFile[preset["title"]] = song_title
+        audio_file[preset["title"]] = song_title
 
     # set album name
-    album_name = songObj.get_album_name()
+    album_name = song_obj.get_album_name()
     if album_name:
-        audioFile[preset["album"]] = album_name
+        audio_file[preset["album"]] = album_name
 
     # set release data
-    release_data = songObj.get_album_release()
+    release_data = song_obj.get_album_release()
     if release_data:
-        audioFile[preset["date"]] = release_data
-        audioFile[preset["originaldate"]] = release_data
+        audio_file[preset["date"]] = release_data
+        audio_file[preset["originaldate"]] = release_data
 
     # set genre
-    genre = songObj.get_genres()[0]
+    genre = song_obj.get_genres()[0]
     if genre:
-        audioFile[preset["genre"]] = genre
+        audio_file[preset["genre"]] = genre
 
     # set disc number
-    disc_number = songObj.get_disc_number()
+    disc_number = song_obj.get_disc_number()
     if disc_number:
         if encoding in ["flac", "ogg", "opus"]:
-            audioFile[preset["discnumber"]] = str(disc_number)
+            audio_file[preset["discnumber"]] = str(disc_number)
         else:
-            audioFile[preset["discnumber"]] = [(disc_number, 0)]
+            audio_file[preset["discnumber"]] = [(disc_number, 0)]
 
     # set track number
-    track_number = songObj.get_track_number()
+    track_number = song_obj.get_track_number()
     if track_number:
         if encoding in ["flac", "ogg", "opus"]:
-            audioFile[preset["tracknumber"]] = str(track_number)
+            audio_file[preset["tracknumber"]] = str(track_number)
         else:
-            audioFile[preset["tracknumber"]] = [(track_number, 0)]
+            audio_file[preset["tracknumber"]] = [(track_number, 0)]
 
 
-def _embed_ogg_metadata(audioFile, songObj):
+def _embed_ogg_metadata(audio_file, song_obj):
     # set year
-    year = songObj.get_album_release().split("-")[0]
+    year = song_obj.get_album_release().split("-")[0]
     if year:
-        audioFile["year"] = year
+        audio_file["year"] = year
 
     # set youtube link as comment
-    youtube_link = songObj.get_youtube_link()
+    youtube_link = song_obj.get_youtube_link()
     if youtube_link:
-        audioFile["comment"] = youtube_link
+        audio_file["comment"] = youtube_link
 
     # set lyrics
-    lyrics = songObj.get_lyrics()
+    lyrics = song_obj.get_lyrics()
     if lyrics:
-        audioFile["lyrics"] = lyrics
+        audio_file["lyrics"] = lyrics
 
 
-def _embed_cover(audioFile, songObj, encoding):
+def _embed_cover(audio_file, song_obj, encoding):
     image = Picture()
     image.type = 3
     image.desc = "Cover"
     image.mime = "image/jpeg"
     image.data = urlopen(
-        songObj.get_album_cover_url()
+        song_obj.get_album_cover_url()
     ).read()
 
     if encoding == "flac":
-        audioFile.add_picture(image)
+        audio_file.add_picture(image)
     elif encoding == "ogg" or encoding == "opus":
         # From the Mutagen docs (https://mutagen.readthedocs.io/en/latest/user/vcomment.html)
         image_data = image.write()
         encoded_data = base64.b64encode(image_data)
         vcomment_value = encoded_data.decode("ascii")
-        audioFile["metadata_block_picture"] = [vcomment_value]
+        audio_file["metadata_block_picture"] = [vcomment_value]
 
 
 SET_ID3_FUNCTIONS = {
@@ -266,7 +266,7 @@ SET_ID3_FUNCTIONS = {
 }
 
 
-def set_id3_data(convertedFilePath, songObj, outputFormat):
+def set_id3_data(converted_file_path, song_obj, outputFormat):
     function = SET_ID3_FUNCTIONS.get(outputFormat)
     if function:
-        function(convertedFilePath, songObj)
+        function(converted_file_path, song_obj)
