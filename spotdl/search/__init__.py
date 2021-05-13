5cf46781759954b0bd405a853f3c272ec8138b4d
phcreery <phcreery@gmail.com>
2021-05-12 19:18:23 -0500

    init cleanup

--- spotdl/search/__init__.py
+++ spotdl/search/__init__.py
@@ -1,5 +1,3 @@
-__all__ = ["provider", "songObj", "spotifyClient", "utils"]
-
 # ! You should be able to do all you want with just theese three lines
 # ! from spotdl.search.spotifyClient    import initialize
 # ! from spotdl.search.songObj          import songObj
