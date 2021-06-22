20faae705d653732fa5b98e1aeb49785c996c114
AZMCode <adrianozambrana@protonmail.com>
2021-06-22 03:01:09 -0400

    Shorter filenames when path is too long (#1233)
    * Added smallerName fallback for long paths
    
    * Update spotdl/download/downloader.py
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>
    
    * Update spotdl/download/downloader.py
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>
    
    * Update spotdl/download/downloader.py
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>
    
    * Delete .tox directory
    
    * First attempt at testing long filenames
    
    It still doesn't work, due to reasons I cannot understand yet.
    I'd appreciate help any day
    
    * Some progress in bugfixing
    Cassette for the "download_long_song" test
    Program works, but tests don't which is mysterious af
    
    * Fixed the relevant test case
    
    Other tests are failing though, and I have no idea why
    
    * All tests check out in linux, with a try block for windows.
    Let's see how this goes.
    
    * Update .gitignore
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>
    
    * Update tests/test_downloader.py
    
    Co-authored-by: Andrzej Klajnert <github@aklajnert.pl>
    
    * Used pytest.raises
    
    * Fixed test
    
    * Used comprehension instead of map
    
    * Fixed typo
    
    * Moved File Path Creation to separate function
    
    * Turned class method into function
    
    * Fixed function definition
    
    * First pass at PEP-8 Compliance
    
    * Final tweaks
    
    * - Futher corrected mistakes.
    - Made _get_smaller_file_path
    - Made _sanitize_filename
    
    * Update spotdl/search/songObj.py
    
    Co-authored-by: Andrzej Klajnert <github@aklajnert.pl>
    
    * Fixed string matching issue
    
    * Fixed flake8 and mypy errors
    
    * Fixed missing .lower() call
    
    * Fixed some broken tests
    
    * Fixed WindowsError throw
    
    * Applied various suggestions by @aklajnert
    
    * Fixed flake8 errors
    
    * Fixed mutable default arguments issue
    by setting argument defaults to None
    
    * Update spotdl/download/downloader.py
    
    * Update spotdl/download/downloader.py
    
    * Update spotdl/download/downloader.py
    
    * Update spotdl/download/downloader.py
    
    * Update spotdl/download/downloader.py
    
    * Update spotdl/download/downloader.py
    
    * Update test_download_long_artists_song.yaml
    
    * Delete test_download_long_name_song.yaml
    
    * fixes
    
    * Update downloader.py
    
    * Update downloader.py
    
    * Update downloader.py
    
    * Update downloader.py
    
    * Update downloader.py
    
    * Update downloader.py
    
    * Update songGatherer.py
    
    * Update test_download_long_artists_song.yaml
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>
    Co-authored-by: Andrzej Klajnert <github@aklajnert.pl>
    Co-authored-by: Jakub Kot <42355410+xnetcat@users.noreply.github.com>

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -9,19 +9,30 @@ from spotdl.search.songObj import SongObj
 from spotdl.download import ffmpeg, downloader
 
 
-def create_song_obj(name="test song", artist="test artist") -> SongObj:
-    artists = [{"name": artist}]
+def create_song_obj(name:str=None, artists_input:list=None) -> SongObj:
+    song_name = None
+    if name == None:
+        song_name = "test song"
+    else:
+        song_name = name
+
+
+    artist_map = None
+    if artists_input == None:
+        artist_objs = list(map(lambda x: {"name": x},["test artist"]))
+    else:
+        artist_objs = list(map(lambda x: {"name": x},artists_input))
     raw_track_meta = {
-        "name": name,
+        "name": song_name,
         "album": {
             "name": "test album",
-            "artists": artists,
+            "artists": artist_objs,
             "release_date": "2021",
             "images": [
                 {"url": "https://i.ytimg.com/vi_webp/iqKdEhx-dD4/hqdefault.webp"}
             ],
         },
-        "artists": artists,
+        "artists": artist_objs,
         "track_number": "1",
         "genres": ["test genre"],
     }
@@ -87,6 +98,29 @@ def test_download_single_song(setup):
     assert [file.basename for file in setup.directory.listdir() if file.isfile()] == [
         "test artist - test song.mp3"
     ]
+
+@pytest.mark.vcr()
+def test_download_long_artists_song(setup):
+    # ! Generates a long list of artists, numbered 1 to 260, to trigger filename length cases
+    artists = [str(i) for i in range(260)]
+    song_obj = create_song_obj(artists_input=artists)
+    with DownloadManager() as dm:
+        dm.download_single_song(song_obj)
+
+    assert [file.basename for file in setup.directory.listdir() if file.isfile()] == [
+        "0 - test song.mp3"
+    ]
+
+@pytest.mark.vcr()
+def test_download_long_name_song(setup):
+    # ! Generates a long title name,numbered 1 to 260, to trigger filename length cases
+    # ! In this case the program cannot save the song, and fails with an OSError
+    song = 'a' * 260
+    song_obj = create_song_obj(name=song)
+
+    with pytest.raises(OSError):
+        with DownloadManager() as dm:
+            dm.download_single_song(song_obj)
 
 
 def test_download_multiple_songs(pytestconfig, setup):
