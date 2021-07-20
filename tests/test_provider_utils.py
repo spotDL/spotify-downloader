4bf7b6d51f304418f7fc97f334631242e5527e6f
Jakub Kot <xnetcat.dev@gmail.com>
2021-07-21 01:51:30 +0200

    misc: renamed tests file

--- tests/test_provider_utils.py
+++ tests/test_provider_utils.py
@@ -1,4 +1,4 @@
-from spotdl.search.audioProvider import _parse_duration
+from spotdl.providers.provider_utils import _parse_duration
 
 
 def test_parse_duration():
@@ -12,5 +12,5 @@ def test_parse_duration():
     assert _parse_duration("25:59:59") == float(93599.0)  # 26 hour result
     assert _parse_duration("likes") == float(0.0)  # bad values
     assert _parse_duration("views") == float(0.0)
-    assert _parse_duration([1, 2, 3]) == float(0.0)
-    assert _parse_duration({"json": "data"}) == float(0.0)
+    assert _parse_duration([1, 2, 3]) == float(0.0)  # type: ignore
+    assert _parse_duration({"json": "data"}) == float(0.0)  # type: ignore
