94ac18c2ecfdc2deeb47350ed0ec7e9a87457888
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-10-20 10:08:18 +0200

    misc improvements (#1392)
    * misc: removed gathering spotify metadata message
    
    * misc: removed debug termination
    
    * misc: bump default search threads to 4 for cli
    
    * Revert "misc: removed debug termination"
    
    This reverts commit 5787beafb28de1f7c152499c5c1c4e1be738a76e.
    
    Co-authored-by: Silverarmor <23619946+Silverarmor@users.noreply.github.com>

--- spotdl/parsers/argument_parser.py
+++ spotdl/parsers/argument_parser.py
@@ -136,7 +136,7 @@ def parse_arguments():
         "--st",
         help="Number of threads used when searching for songs",
         type=int,
-        default=1,
+        default=4,
     )
 
     # Option to generate .m3u
