dac1fdb026aa012df94c13564e66cf88df2cf1af
Jakub Kot <xnetcat.dev@gmail.com>
2022-04-14 17:54:24 +0200

    init tests for yt music

---
+++ tests/providers/audio/test_ytmusic.py
@@ -0,0 +1,23 @@
+import pytest
+
+from spotdl.providers.audio import YouTubeMusic
+from spotdl.types.song import Song
+
+
+@pytest.mark.vcr()
+def test_find_isrc_song():
+    provider = YouTubeMusic()
+
+    results = provider.get_results("GB2LD2210068")
+
+    assert results[0]["link"] == "https://youtube.com/watch?v=0h6XAAwX8II"
+
+
+def test_find_song():
+    provider = YouTubeMusic()
+
+    song = Song.from_search_term("Lost Identities Moments")
+
+    results = provider.search(song)
+
+    assert results == "https://youtube.com/watch?v=0h6XAAwX8II"
