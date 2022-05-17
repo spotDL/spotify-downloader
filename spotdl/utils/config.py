8f7cae562c4aeeda9c0b830998346e7efb569621
Jakub Kot <xnetcat.dev@gmail.com>
2022-05-17 19:00:16 +0200

    added genius as default fallback lyrics provider

--- spotdl/utils/config.py
+++ spotdl/utils/config.py
@@ -120,7 +120,7 @@ DEFAULT_CONFIG = {
     "simple_tui": False,
     "cache_path": str(get_cache_path()),
     "audio_providers": ["youtube-music"],
-    "lyrics_providers": ["musixmatch"],
+    "lyrics_providers": ["musixmatch", "genius"],
     "ffmpeg": "ffmpeg",
     "bitrate": None,
     "ffmpeg_args": None,
