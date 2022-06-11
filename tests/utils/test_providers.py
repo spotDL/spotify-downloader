17083b9e3e7a71d28fede9853714f586e6c4930f
Jakub Kot <xnetcat.dev@gmail.com>
2022-06-11 21:50:11 +0200

    added tests for `__init__.py`

--- tests/utils/test_providers.py
+++ tests/utils/test_providers.py
@@ -1,12 +1,28 @@
+import pytest
+
 from spotdl.utils.providers import match_percentage
 
 
-def test_match_percentage():
+@pytest.mark.parametrize(
+    "str1, str2, score_cutoff, expected",
+    [
+        ("", "", 0, 100.0),
+        ("test", "test", 0, 100.0),
+        ("test", "test", 20, 100.0),
+        ("test", "test", 101.0, 0.0),
+        ("test", "💩", 0, 0.0),
+        (None, None ,0, 0.0),
+        ("test", None, 0, 0.0),
+        (123, "test", 0, 0.0),
+    ]
+)
+def test_match_percentage(str1, str2, score_cutoff, expected):
     """
     Test match_percentage function
     """
 
-    assert match_percentage("test", "test") == 100.0
-    assert match_percentage("test", "test", score_cutoff=0.5) == 100.0
-    assert match_percentage("test", "test", score_cutoff=101.0) == 0.0
-    assert match_percentage("test", "💩") == 0.0
+    if isinstance(str1, int):
+        with pytest.raises(TypeError):
+            match_percentage(str1, str2, score_cutoff) # type: ignore
+    else:
+        assert match_percentage(str1, str2, score_cutoff) == expected
