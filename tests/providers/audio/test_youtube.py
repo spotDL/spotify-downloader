f2b3c18e95890c0fc651fc55e95a31c3354fa6a0
Jakub Kot <xnetcat.dev@gmail.com>
2022-06-11 22:20:37 +0200

    added basic tests for audio providers

--- tests/providers/audio/test_youtube.py
+++ tests/providers/audio/test_youtube.py
@@ -1,22 +1,23 @@
 import pytest
 
-from spotdl.providers.audio import YouTubeMusic
+from spotdl.providers.audio.youtube import YouTube
 from spotdl.types.song import Song
 
-
 @pytest.mark.vcr()
-def test_ytm_search():
-    provider = YouTubeMusic()
+def test_yt_search():
+    provider = YouTube()
 
     assert (
         provider.search(Song.from_search_term("Lost Identities Moments"))
-        == "https://youtube.com/watch?v=0h6XAAwX8II"
+        == "https://youtube.com/watch?v=14CjyUYO8lE"
     )
 
+
 @pytest.mark.vcr()
-def test_ytm_get_results():
-    provider = YouTubeMusic()
+def test_yt_get_results():
+    provider = YouTube()
 
     results = provider.get_results("Lost Identities Moments")
 
-    assert len(results) > 5
+    assert results and len(results) > 5
+
