96fcbd2e3104a626e1922b247c589acbcb247c7a
MrFellox <jfernandohernandez28@gmail.com>
2022-09-14 09:15:27 -0600

    set_lyrics now applies lyrics to mp3 files
    - Instead of using filename for the song, we now use the song's metadata
    - Added some nice colorful console logs for the user

--- spotdl/console/lyrics.py
+++ spotdl/console/lyrics.py
@@ -5,13 +5,14 @@ Sync Lyrics module for the console
 import sys
 import os
 from pathlib import Path
-from mutagen.easyid3 import ID3
+from rich.console import Console
+from mutagen.easyid3 import ID3, EasyID3
 from mutagen.id3 import USLT
+from typing import List
 
 from spotdl.providers.lyrics import Genius, AzLyrics, MusixMatch
 from spotdl.providers.lyrics.base import LyricsProvider
 
-from typing import List
 
 providers: List[LyricsProvider] = [
     Genius(),
@@ -21,65 +22,49 @@ providers: List[LyricsProvider] = [
 
 # TODO: Get default lyrics provider or ask the user to pass in a provider. Or try with them until one returns the lyrics.
 def set_lyrics(path: Path):
-    print("Lyrics operation was ran.")
 
+    console = Console()
     if path.is_file():
-        # (
-        #     path.endswith(".mp3")
-        #     or path.endswith(".m4a")
-        #     or path.endswith(".ogg")
-        #     or path.endswith(".flac")
-        #     or path.endswith(".opus")
-        # ):
+        # Get song data with file metadata
+        song_file = EasyID3(str(path.resolve()))
+        song_name = song_file.get("title")[0]
+        song_artists = parse_artists(song_file.get("artist")[0])
+        song_file.save()
 
-        # Search with filename
-        print("Getting song lyrics...")
-        # Song filenames should be in format artist1, artist2 - song name.extension
-        # TODO: Possibly search the song metadata and get artitst and song name, and not depend in filename
+        with console.status(
+            f"[bold]Getting lyrics for [bold green]{song_name}[/bold green][/bold] by [bold green]{song_artists[0]}[/bold green]"
+        ):
 
-        filename = os.path.splitext(path.name)[0]
-        song_data = filename.split("-")
+            lyrics = None
+            for provider in providers:
+                lyrics = provider.get_lyrics(name=song_name, artists=song_artists)
 
-        # Parse song name if there are any hypens in the name of the song
-        # TODO: Get song name from the file's metadata.
-        song_name = ""
-        i = 0
-        print("////")
-        for entry in song_data:
+                if not lyrics:
+                    continue
+                break
 
-            if entry == song_data[0]:
-                continue
+        if lyrics:
+            with console.status("[bold bright_yellow] Setting lyrics..."):
+                if path.name.endswith(".mp3"):
+                    song_file = ID3(str(path.resolve()))
+                    song_file["USLT::'eng'"] = USLT(
+                        encoding=3, lang="eng", desc="desc", text=lyrics
+                    )
+                    song_file.save(v2_version=3)
+                    console.print(
+                        f"[bold green]sSuccesfully applied lyrics to [/ bold green] [bold bright_yellow]{song_name}[/bold bright_yellow] by [bold bright_yellow]{song_artists[0]}[/bold bright_yellow]."
+                    )
+                    return
 
-            if i == 0:
-                song_name += entry.strip()
-                i += 1
-                continue
-
-            song_name += f"-{entry.strip()}"
-
-        song_name.strip()
-        artists = [artist.strip() for artist in song_data[0].strip().split(",")]
-
-        for provider in providers:
-            print(
-                f'Attempting to find lyrics with artists {artists} and song name: "{song_name}" with {type(provider).__name__}...'
-            )
-
-            lyrics = MusixMatch().get_lyrics(name=song_name, artists=artists)
-
-            if not lyrics:
-                print("Could not find lyrics. Re trying...")
-                continue
-
-            print(lyrics)
-            break
-
-        # if lyrics:
-        #     if path.name.endswith(".mp3"):
-        #         song_file = ID3(str(path.resolve()))
-
-        #         song_file["USLT::'eng'"] = USLT(
-        #             encoding=3, lang="eng", desc="desc", text=lyrics
-        #         )
-
+        console.print(
+            f"[bold red]Could not find lyrics for [bold bright_yellow]{song_name}."
+        )
     return None
+
+
+def parse_artists(artists: str) -> List[str]:
+    """
+    Parses the artists from a string into a List.
+    """
+    artist_list = [artist.strip() for artist in artists.strip().split("/")]
+    return artist_list
