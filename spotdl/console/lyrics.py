cde7894993d8c25ebb0d4af53799c3a63b6cd61d
MrFellox <jfernandohernandez28@gmail.com>
2022-09-14 07:43:25 -0600

    now set_lyrics tries with all lyrics providers

--- spotdl/console/lyrics.py
+++ spotdl/console/lyrics.py
@@ -2,20 +2,26 @@
 Sync Lyrics module for the console
 """
 
-from fileinput import filename
 import sys
 import os
 from pathlib import Path
-
 from mutagen.easyid3 import ID3
 from mutagen.id3 import USLT
 
 from spotdl.providers.lyrics import Genius, AzLyrics, MusixMatch
+from spotdl.providers.lyrics.base import LyricsProvider
 
+from typing import List
+
+providers: List[LyricsProvider] = [
+    Genius(),
+    AzLyrics(),
+    MusixMatch(),
+]
 
 # TODO: Get default lyrics provider or ask the user to pass in a provider. Or try with them until one returns the lyrics.
 def set_lyrics(path: Path):
-    print("set_lyrics ran")
+    print("Lyrics operation was ran.")
 
     if path.is_file():
         # (
@@ -25,17 +31,14 @@ def set_lyrics(path: Path):
         #     or path.endswith(".flac")
         #     or path.endswith(".opus")
         # ):
-        print("is file")
 
         # Search with filename
-        print("getting song lyrics")
-
+        print("Getting song lyrics...")
         # Song filenames should be in format artist1, artist2 - song name.extension
         # TODO: Possibly search the song metadata and get artitst and song name, and not depend in filename
 
         filename = os.path.splitext(path.name)[0]
         song_data = filename.split("-")
-        print(song_data)
 
         # Parse song name if there are any hypens in the name of the song
         # TODO: Get song name from the file's metadata.
@@ -43,14 +46,11 @@ def set_lyrics(path: Path):
         i = 0
         print("////")
         for entry in song_data:
-            print(f"Current entry: {entry}")
 
             if entry == song_data[0]:
-                print("entry is first, element (artists). skip...")
                 continue
 
             if i == 0:
-                print("First word of the song, dont add a hypgen before.")
                 song_name += entry.strip()
                 i += 1
                 continue
@@ -60,13 +60,20 @@ def set_lyrics(path: Path):
         song_name.strip()
         artists = [artist.strip() for artist in song_data[0].strip().split(",")]
 
-        print(
-            f'Attempting to find lyrics with artists {artists} and song name: "{song_name}"...'
-        )
+        for provider in providers:
+            print(
+                f'Attempting to find lyrics with artists {artists} and song name: "{song_name}" with {type(provider).__name__}...'
+            )
 
-        lyrics = MusixMatch().get_lyrics(name=song_name, artists=artists)
+            lyrics = MusixMatch().get_lyrics(name=song_name, artists=artists)
 
-        print(lyrics)
+            if not lyrics:
+                print("Could not find lyrics. Re trying...")
+                continue
+
+            print(lyrics)
+            break
+
         # if lyrics:
         #     if path.name.endswith(".mp3"):
         #         song_file = ID3(str(path.resolve()))
