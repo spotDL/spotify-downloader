5a53668be1b82cdcaa9336e1e221bf4e07ef5f69
Jakub Kot <xnetcat.dev@gmail.com>
2022-10-22 20:00:07 +0200

    merged imports in types

--- spotdl/types/saved.py
+++ spotdl/types/saved.py
@@ -4,8 +4,7 @@ Saved module for handing the saved tracks from user library
 
 from dataclasses import dataclass
 from typing import Any, Dict, List
-from spotdl.types.song import SongList
-from spotdl.types.song import Song
+from spotdl.types.song import SongList, Song
 from spotdl.utils.spotify import SpotifyClient
 
 
