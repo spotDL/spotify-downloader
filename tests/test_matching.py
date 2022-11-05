27a5e741abc307490c51f04e7d81fefe6ee52a3f
Jakub Kot <xnetcat.dev@gmail.com>
2022-11-05 14:57:27 +0100

    removed redundant test

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -5,15 +5,27 @@ from spotdl.providers.audio.ytmusic import YouTubeMusic
 from spotdl.utils.spotify import SpotifyClient
 from tests.conftest import new_initialize
 
+
 @pytest.mark.parametrize(
     "query, expected",
     [
-        ("https://open.spotify.com/track/0l9XhUIYk2EjT6MHdh4wJU", "https://music.youtube.com/watch?v=TIULUkt30Os"),
-        ("https://open.spotify.com/track/1OdYXTMwjl4f4g4ch05CEq", "https://www.youtube.com/watch?v=5uwTXzxOseg"),
-        ("https://open.spotify.com/track/0QsZ3W21xNvnUnUMbiAY4z", "https://www.youtube.com/watch?v=UlYwnX5DO2Y"),
-        ("https://open.spotify.com/track/1zi7xx7UVEFkmKfv06H8x0", "https://www.youtube.com/watch?v=5LFB3qdmZBM"),
-
-    ]
+        (
+            "https://open.spotify.com/track/0l9XhUIYk2EjT6MHdh4wJU",
+            "https://music.youtube.com/watch?v=TIULUkt30Os",
+        ),
+        (
+            "https://open.spotify.com/track/1OdYXTMwjl4f4g4ch05CEq",
+            "https://music.youtube.com/watch?v=5uwTXzxOseg",
+        ),
+        (
+            "https://open.spotify.com/track/0QsZ3W21xNvnUnUMbiAY4z",
+            "https://www.youtube.com/watch?v=UlYwnX5DO2Y",
+        ),
+        (
+            "https://open.spotify.com/track/1zi7xx7UVEFkmKfv06H8x0",
+            "https://www.youtube.com/watch?v=5LFB3qdmZBM",
+        ),
+    ],
 )
 def test_ytmusic_matching(monkeypatch, query, expected):
     monkeypatch.setattr(SpotifyClient, "init", new_initialize)
