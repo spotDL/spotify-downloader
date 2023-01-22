spotDL/spotify-downloader

commit d0c3743d23860adc312d340e5e858bd5c3d1b7c0

stop metadata fetching 2x more than necessary

spotdl/download/downloader.py
@@ -227,11 +227,11 @@ class Downloader:
         """
 
         if self.settings["fetch_albums"]:
-            albums = set(
-                str(Song.from_url(song.url).album_id)
-                for song in songs
-                if Song.from_url(song.url).album_id is not None
-            )
+            albums = set()
+            for song in songs:
+                songmeta = Song.from_url(song.url)
+                if songmeta.album_id is not None:
+                    albums.add(songmeta.album_id)
             logger.info(
                 "Fetching %d album%s", len(albums), "s" if len(albums) > 1 else ""
             )

commit b001b8e303f405c4ccd28b853621e23999effdd8

never mind d0c3743 broke it, reverting

spotdl/download/downloader.py
@@ -227,11 +227,7 @@ class Downloader:
         """
 
         if self.settings["fetch_albums"]:
-            albums = set()
-            for song in songs:
-                songmeta = Song.from_url(song.url)
-                if songmeta.album_id is not None:
-                    albums.add(songmeta.album_id)
+            albums = set(str(Song.from_url(song.url).album_id) for song in songs if Song.from_url(song.url).album_id is not None)
             logger.info(
                 "Fetching %d album%s", len(albums), "s" if len(albums) > 1 else ""
             )