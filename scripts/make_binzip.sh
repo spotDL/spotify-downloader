77b9f32cad0d2729ab2e08d2c55849e615c84ae8
Gabriel Fontán <60316747+BobbyESP@users.noreply.github.com>
2023-02-27 17:33:01 +0100

    Added bash script for building bin-zip file that contains the library (#1747)

---
+++ scripts/make_binzip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CODE_FOLDERS="spotdl spotdl/console spotdl/download spotdl/providers spotdl/providers/audio spotdl/providers/lyrics spotdl/types spotdl/utils"
+PYTHON="/usr/bin/env python3"
+
+mkdir -p zip
+
+for d in $CODE_FOLDERS; do
+    mkdir -p "zip/$d"
+    cp -pPR "$d"/*.py "zip/$d/"
+done
+
+touch -t 200001010101 zip/spotdl/*.py zip/spotdl/**/*.py
+mv zip/spotdl/__main__.py zip/
+cd zip
+zip -q -r spotdl spotdl/**.py spotdl/**/* spotdl/**/*.py __main__.py
+rm -rf zip
+
+echo '#!'"$PYTHON" > spotdl_bin
+cat spotdl.zip >> spotdl_bin
+rm spotdl.zip
+chmod a+x spotdl_bin
