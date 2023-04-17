451950fe59db4a6c443859ff871edd0cd6511dd4
kuba <xnetcat.dev@gmail.com>
2023-04-17 20:25:36 +0200

    re-added slider-kz to audio providers

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -18,6 +18,7 @@ from yt_dlp.postprocessor.sponsorblock import SponsorBlockPP
 
 from spotdl.download.progress_handler import ProgressHandler
 from spotdl.providers.audio import AudioProvider, YouTube, YouTubeMusic
+from spotdl.providers.audio.sliderkz import SliderKZ
 from spotdl.providers.lyrics import AzLyrics, Genius, LyricsProvider, MusixMatch, Synced
 from spotdl.types.options import DownloaderOptionalOptions, DownloaderOptions
 from spotdl.types.song import Song
@@ -46,6 +47,7 @@ __all__ = [
 AUDIO_PROVIDERS: Dict[str, Type[AudioProvider]] = {
     "youtube": YouTube,
     "youtube-music": YouTubeMusic,
+    "slider-kz": SliderKZ,
 }
 
 LYRICS_PROVIDERS: Dict[str, Type[LyricsProvider]] = {
