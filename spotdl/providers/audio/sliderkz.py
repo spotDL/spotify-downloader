e4c76679a171f4cea3c69f8b2ac878ae185cdf19
kuba <xnetcat.dev@gmail.com>
2023-04-17 20:26:18 +0200

    simplified slider.kz audio provider

--- spotdl/providers/audio/sliderkz.py
+++ spotdl/providers/audio/sliderkz.py
@@ -1,15 +1,22 @@
+"""
+SliderKZ module for downloading and searching songs.
+"""
+
+import logging
 from typing import Any, Dict, List
 
-import logging  # pylint: disable=C0114
-import json
 import requests
-
 
 from spotdl.providers.audio.base import AudioProvider
 from spotdl.types.result import Result
 
 __all__ = ["SliderKZ"]
+
 logger = logging.getLogger(__name__)
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"
+}
 
 
 class SliderKZ(AudioProvider):
@@ -33,55 +40,53 @@ class SliderKZ(AudioProvider):
         - A list of slider.kz results if found, None otherwise.
         """
 
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"
-        }
-        search_results = ""
+        search_results = None
         max_retries = 0
-        results = []
 
-        while max_retries < 3:
+        while not search_results and max_retries < 3:
             try:
-                search_results = requests.get(
+                search_response = requests.get(
                     url="https://slider.kz/vk_auth.php?q=" + search_term,
-                    headers=headers,
+                    headers=HEADERS,
                     timeout=5,
                 )
 
-                if (
-                    len(search_results.text) < 30
-                ):  # sometimes slider.kz returns an empty string
-                    max_retries += 1
-                    raise ConnectionError
+                # Check if the response is valid
+                if len(search_response.text) > 30:
+                    # Set the search results to the json response
+                    # effectively breaking out of the loop
+                    search_results = search_response.json()
 
-                search_results = json.loads(search_results.text)
-                break
-            except ConnectionError:
-                max_retries += 1
-        else:
-            logger.error("Failed to get results from slider.kz")
-            return results
-
-        try:
-            for result in search_results["audios"][""]:
-                # urls from slider.kz sometimes are relative, so we need to add the domain
-                if "https://" not in result["url"]:
-                    result["url"] = "https://slider.kz/" + result["url"]
-
-                results.append(
-                    Result(
-                        source="slider.kz",
-                        url=result["url"],
-                        verified=False,
-                        name=str(result["tit_art"]),
-                        duration=int(result["duration"]) * 1000,
-                        author="slider.kz",
-                        result_id=result["id"],
-                        views=1,
-                    )
+            except Exception as exc:
+                logger.debug(
+                    "Slider.kz search failed for query %s with error: %s. Retrying...",
+                    search_term,
+                    exc,
                 )
 
-        except (KeyError, TypeError, ValueError) as exception:
-            logger.error("Failed with exception: %s", exception)
+            max_retries += 1
+
+        if not search_results:
+            logger.debug("Slider.kz search failed for query %s", search_term)
+            return []
+
+        results = []
+        for result in search_results["audios"][""]:
+            # urls from slider.kz sometimes are relative, so we need to add the domain
+            if "https://" not in result["url"]:
+                result["url"] = "https://slider.kz/" + result["url"]
+
+            results.append(
+                Result(
+                    source="slider.kz",
+                    url=result.get("url"),
+                    verified=False,
+                    name=result.get("tit_art"),
+                    duration=int(result.get("duration", -9999)),
+                    author="slider.kz",
+                    result_id=result.get("id"),
+                    views=1,
+                )
+            )
 
         return results
