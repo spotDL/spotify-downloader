d29385535508a000e322c843943b38ea6a8a13c2
kuba <xnetcat.dev@gmail.com>
2023-06-13 00:52:47 +0200

    added support for piped links

--- spotdl/utils/search.py
+++ spotdl/utils/search.py
@@ -128,7 +128,7 @@ def get_simple_songs(
         request = re.sub(r"\/intl-\w+\/", "/", request)
 
         if (
-            ("youtube.com/watch?v=" in request or "youtu.be/" in request)
+            ("watch?v=" in request or "youtu.be/" in request)
             and "open.spotify.com" in request
             and "track" in request
             and "|" in request
@@ -136,7 +136,7 @@ def get_simple_songs(
             split_urls = request.split("|")
             if (
                 len(split_urls) <= 1
-                or not ("youtube" in split_urls[0] or "youtu.be" in split_urls[0])
+                or not ("watch?v=" in split_urls[0] or "youtu.be" in split_urls[0])
                 or "spotify" not in split_urls[1]
             ):
                 raise QueryError(
