f24c8122cc240f23b2a619dfad6ed664aae622d2
Alexandre Truppel <alex.truppel@gmail.com>
2024-10-01 19:54:28 +0200

    Simplified

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -308,9 +308,8 @@ class Downloader:
             with open(
                 self.settings["save_errors"], "a", encoding="utf-8"
             ) as error_file:
-                if len(self.errors) > 0 and error_file.tell() > 0:
-                    error_file.write("\n")
-                error_file.write("\n".join(self.errors))
+                for error in self.errors:
+                    error_file.write(f"{error}\n")
 
             logger.info("Saved errors to %s", self.settings["save_errors"])
 
