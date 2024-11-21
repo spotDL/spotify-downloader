8a5825a954ec1a89b521eaa8b1a701d1ba656cd0
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2024-11-21 19:29:11 +1300

    syntax fix

--- spotdl/utils/metadata.py
+++ spotdl/utils/metadata.py
@@ -444,8 +444,12 @@ def get_file_metadata(path: Path, id3_separator: str = "/") -> Optional[Dict[str
 
                 continue
 
-            if path.suffix == ".flac" and len(audio_file.pictures) > 0:
-                song_meta["album_art"] = audio_file.pictures[0].data
+            if path.suffix == ".flac":
+                if audio_file.pictures:
+                    song_meta["album_art"] = audio_file.pictures[0].data
+                else:
+                    song_meta["album_art"] = None
+
                 continue
 
             if path.suffix in [".ogg", ".opus"]:
