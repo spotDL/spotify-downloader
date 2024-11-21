8d90709fa23874523e107928f21f2786f24d10df
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2024-11-21 19:08:00 +1300

    syntax: if statement 1 line

--- spotdl/utils/metadata.py
+++ spotdl/utils/metadata.py
@@ -444,9 +444,8 @@ def get_file_metadata(path: Path, id3_separator: str = "/") -> Optional[Dict[str
 
                 continue
 
-            if path.suffix == ".flac":
-                if len(audio_file.pictures) > 0:
-                    song_meta["album_art"] = audio_file.pictures[0].data
+            if path.suffix == ".flac" and len(audio_file.pictures) > 0:
+                song_meta["album_art"] = audio_file.pictures[0].data
                 continue
 
             if path.suffix in [".ogg", ".opus"]:
