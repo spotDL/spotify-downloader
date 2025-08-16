77ae0aa9546a135c2c0a3c38471a16bc5a547259
Jayden <23619946+Silverarmor@users.noreply.github.com>
2025-08-16 14:01:52 +1200

    Prevent duplicate m3u entries on detect_formats

--- spotdl/utils/m3u.py
+++ spotdl/utils/m3u.py
@@ -59,7 +59,8 @@ def create_m3u_content(
                 if file_name.exists():
                     text += str(file_name) + "\n"
                     break
-
+            else:
+                # Runs if no existing file was found (no break)
                 file_name = create_file_name(
                     song, template, file_extension, restrict, short
                 )
