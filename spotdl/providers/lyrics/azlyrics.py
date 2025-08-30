76cc4dc1be734ba204d65333b70eaf5044220b8b
Jayden <23619946+Silverarmor@users.noreply.github.com>
2025-08-30 12:37:39 +1200

    Black formatting

--- spotdl/providers/lyrics/azlyrics.py
+++ spotdl/providers/lyrics/azlyrics.py
@@ -28,8 +28,8 @@ class AzLyrics(LyricsProvider):
             {
                 "Host": "www.azlyrics.com",
                 "User-Agent": (
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                    '(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 /CefSharp Browser 90.0'
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                    "(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 /CefSharp Browser 90.0"
                 ),
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
