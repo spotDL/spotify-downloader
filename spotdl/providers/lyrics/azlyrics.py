d7dc27fae35673d1d9c9e3df082e8a685549e401
Jayden <23619946+Silverarmor@users.noreply.github.com>
2025-08-31 12:42:13 +1200

    Update user-agent

--- spotdl/providers/lyrics/azlyrics.py
+++ spotdl/providers/lyrics/azlyrics.py
@@ -29,7 +29,7 @@ class AzLyrics(LyricsProvider):
                 "Host": "www.azlyrics.com",
                 "User-Agent": (
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 /CefSharp Browser 90.0"
+                    "(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
                 ),
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
