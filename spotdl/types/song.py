2ecf00edb89fe37c8c41904332802db8534ab3a8
Jayden <23619946+Silverarmor@users.noreply.github.com>
2026-08-12 16:01:14 +1200

    Update spotdl/types/song.py

--- spotdl/types/song.py
+++ spotdl/types/song.py
@@ -78,6 +78,7 @@ class Song:
         if "open.spotify.com" not in url or "track" not in url:
             raise SongError(f"Invalid URL: {url}")
         url = re.sub(r"\/intl-\w+\/", "/", url)
+
         # query spotify for song, artist, album details
         spotify_client = SpotifyClient()
 
