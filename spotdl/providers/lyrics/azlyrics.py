a522de857471ad7f6258116684e676785f3b2a5e
iPaulis <iPaulis@users.noreply.github.com>
2026-08-22 23:14:44 +0200

    azlyrics: drop the Host header that causes a redirect loop

--- spotdl/providers/lyrics/azlyrics.py
+++ spotdl/providers/lyrics/azlyrics.py
@@ -26,10 +26,9 @@ class AzLyrics(LyricsProvider):
         self.session = requests.Session()
         # No User-Agent override: AZLyrics traps requests claiming to be a
         # browser in a redirect loop, while requests' default UA is served
-        # normally (see #2682).
+        # normally (see #2682). A `Host` header also causes a redirect loop.
         self.session.headers.update(
             {
-                "Host": "www.azlyrics.com",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "Accept-Encoding": "gzip, deflate, br, zstd",
